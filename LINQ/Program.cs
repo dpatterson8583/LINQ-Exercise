b67ab9a66f2e9c9a65a95b2f57240d8b71ddff0c
dpatterson8583 <dpatterson8583@gmail.com>
2023-04-29 23:30:57 -0500

    Initial Commit for LINQ Exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace LINQ
+﻿using System.Linq;
+using System;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videogames = new List<string>();
+            List<string> newgames = new List<string>();
+
+            videogames.Add("Assassin's Creed");
+            videogames.Add("Doom");
+            videogames.Add("FortNight");
+            videogames.Add("Tomb Raider");
+            videogames.Add("Stryker");
+            
+
+            newgames = videogames.OrderBy(x => x.Length).ToList();
+
+            foreach(string game in videogames) 
+            {
+                Console.WriteLine(game);           
+            }
+
+            Console.WriteLine("-------------------------------------");
+            foreach (string game in newgames)
+            {
+                Console.WriteLine(game);
+            }
+            
         }
     }
 }
